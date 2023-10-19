8823d018b3854762152cf4f81e17b51cd865c968
Grizzly21f <taras.firhuk72@gmail.com>
2023-10-20 01:50:39 +0300

    HW2-2

--- src/components/LaunchesContainer/Launches/Launches.js
+++ src/components/LaunchesContainer/Launches/Launches.js
@@ -1,40 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import LaunchList from "../Launch/LaunchList";
+
 
 function SpaceXLaunches() {
     const [launches, setLaunches] = useState([]);
 
     useEffect(() => {
-
-        axios.get('https://api.spacexdata.com/v3/launches/')
+        axios
+            .get('https://api.spacexdata.com/v3/launches/')
             .then((response) => {
-
-                const filteredLaunches = response.data.filter(launch => !launch.launch_date_local.includes('2020'));
+                const filteredLaunches = response.data.filter(
+                    (launch) => !launch.launch_date_local.includes('2020')
+                );
                 setLaunches(filteredLaunches);
             })
-            .catch((error) => {
-                console.error('Помилка при отриманні даних з API SpaceX', error);
-            });
     }, []);
 
     return (
         <div>
-            <ul>
-                {launches.map((launch, index) => (
-                    <li key={index}>
-                        <p>Місія: {launch.mission_name}</p>
-                        <p>Дата запуску: {launch.launch_date_local}</p>
-                        <p>Ракета: {launch.rocket.rocket_name}</p>
-                        <p>Сайт запуску: {launch.launch_site.site_name_long}</p>
-                        {launch.links.mission_patch_small && (
-                            <img
-                                src={launch.links.mission_patch_small}
-                                alt={`Патч місії ${launch.mission_name}`}
-                            />
-                        )}
-                    </li>
-                ))}
-            </ul>
+            <h1>Запуски SpaceX (окрім 2020 року)</h1>
+            <LaunchList launches={launches} />
         </div>
     );
 }
